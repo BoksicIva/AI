c5e2ad6e165127cb6f32ce51944ecf6c31b96f0d
BoksicIva <iva.boksic099@gmail.com>
2021-04-05 14:31:59 +0200

    dijkstra added

--- Labos_01/solution.py
+++ Labos_01/solution.py
@@ -1,18 +1,16 @@
 import sys
-from decimal import Decimal
-from pathlib import Path
 from queue import PriorityQueue
-from collections import OrderedDict
-import time
+import collections
+import numpy as np
 
 
 class Node:
-    def __init__(self, name, path, total_cost, h_value, neigbours):
+    def __init__(self, name, path, total_cost, h_value, neighbours):
         self.name = name
         self.path = path
         self.total_cost = total_cost
         self.h_value = h_value
-        self.neigbours = neigbours
+        self.neighbours = neighbours
 
     def __lt__(self, other):
         return self.name < other.name
@@ -32,42 +30,20 @@ class Transition:
         return self.node.name < other.node.name
 
 
-class Timer:
-    def __init__(self):
-        self.counter = 0
-        self.sumTime = 0.0
-
-    def startF(self):
-        self.start = time.time()
-        self.counter += 1
-
-    def endF(self):
-        self.end = time.time()
-        self.sumTime = self.sumTime + self.end - self.start
-
-    def sumAllTime(self):
-        print(self.sumTime)
-
-
 def heuristic(path2):
-    hueristic_values = {}
+    heuristic_values = {}
     with open(path2, encoding='utf-8') as file:
         # lines = file.read().splitlines()
         for line in file:
             if not line.startswith('#'):
                 x = line.split()
-                hueristic_values[x[0][:len(x[0]) - 1]] = x[1]
-    return hueristic_values
+                heuristic_values[x[0][:len(x[0]) - 1]] = x[1]
+    return heuristic_values
 
 
 def transition(path1, heuristics):
     transitions = {}
     counter = 0
-    lista = []
-    timer2 = Timer()
-    timer = Timer()
-    timer.startF()
-    timer3 = Timer()
     with open(path1, encoding="UTF-8") as file:
         for line in file:
 
@@ -90,52 +66,41 @@ def transition(path1, heuristics):
                     for i in range(1, len(x)):
                         name_and_cost = x[i].split(',')
 
-                        nodeExists = transitions.get(name_and_cost[0], False)
-                        timer2.startF()
-                        if nodeExists == False:
+                        node_exists = transitions.get(name_and_cost[0], False)
+                        if not node_exists:
                             nextNode = Node(name_and_cost[0], name_and_cost[0], 0, 0, [])
                             if heuristics:
                                 nextNode.h_value = heuristics[name_and_cost[0]]
-                            newTransition = Transition(nextNode, name_and_cost[1],0,name_and_cost[0])
-                            transitions[name_and_cost[0]] =nextNode
+                            new_transition = Transition(nextNode, name_and_cost[1], 0, name_and_cost[0])
+                            transitions[name_and_cost[0]] = nextNode
                         else:
-                            newTransition = Transition(nodeExists, name_and_cost[1],0,name_and_cost[0])
+                            new_transition = Transition(node_exists, name_and_cost[1], 0, name_and_cost[0])
 
-                        cities.append(newTransition)
-                        timer2.endF()
+                        cities.append(new_transition)
 
-                    timer3.startF()
-                    cities = sorted(cities) ## provjeri suta koliko se vrti
-                    timer3.endF()
+                    cities = sorted(cities)
 
-                    mainNode = transitions.get(state, False)
-                    if mainNode:
-                        mainNode.neigbours = cities
+                    main_node = transitions.get(state, False)
+                    if main_node:
+                        main_node.neighbours = cities
                     else:
 
-                        mainNode = Node(state, state, 0, 0, cities)
+                        main_node = Node(state, state, 0, 0, cities)
                         if heuristics:
-                            mainNode.h_value = heuristics[state]
-                    transitions[state] = mainNode
+                            main_node.h_value = heuristics[state]
+                    transitions[state] = main_node
                     if state == start:
-                        startNode = mainNode
-
-    timer.endF()
-    timer2.sumAllTime()
-    timer.sumAllTime()
-    timer3.sumAllTime()
+                        start_node = main_node
     end_dict = {}
     for city in end:
         end_dict[city] = city
-    return startNode, end_dict, transitions
+    return start_node, end_dict, transitions
 
 
-
-def bfs(s0, suc, goal):
+def bfs(s0, goal):
     open = []
     open.append(s0)
     visited = {}
-    counter = 1
 
     while len(open) != 0:
         n = open.pop(0)
@@ -145,22 +110,21 @@ def bfs(s0, suc, goal):
         if n.name in goal:
             return n, visited
 
-        expand = n.neigbours
+        expand = n.neighbours
         for trans in expand:
             if trans.node.name not in visited:
                 trans.node.path = n.path + " => " + trans.node.name
                 trans.node.total_cost = n.total_cost + int(trans.cost)
                 open.append(trans.node)
-        counter += 1
 
     return False, False
 
 
-def ucs(s0, suc, goal):
+def ucs(s0, goal):
     open = PriorityQueue()
     open.put((0, s0, s0.name))
-    visited= {}
-    while open:
+    visited = {}
+    while open.qsize() != 0:
         all = open.get()
         n = all[1]
         n.total_cost = all[0]
@@ -179,24 +143,23 @@ def ucs(s0, suc, goal):
         if n.name in goal:
             return n, visited
 
-        expand = n.neigbours
+        expand = n.neighbours
 
         for trans in expand:
             if trans.node.name not in visited:
                 trans.path = n.path + " => " + trans.node.name
                 trans.total_cost = n.total_cost + int(trans.cost)
                 open.put((trans.total_cost, trans.node, trans.path))
-
     return False, False
 
 
-def astar(s0, suc, goal, h):
+def astar(s0, goal):
     open = PriorityQueue()
     open.put((0, s0, s0.name, 0))
-    open_dict={}
-    open_dict[s0.name]=s0.name
+    open_dict = {}
+    open_dict[s0.name] = s0.name
     visited = {}
-    while open:
+    while open.qsize() != 0:
         all = open.get()
         n = all[1]
         n.total_cost = float(all[3])
@@ -212,14 +175,14 @@ def astar(s0, suc, goal, h):
         visited[n.name] = n.name
         if n.name in goal:
             return n, visited
-        expand = n.neigbours
+        expand = n.neighbours
         for trans in expand:
             if trans.node.name not in visited:
                 trans.path = n.path + " => " + trans.node.name
                 trans.total_cost = n.total_cost + int(trans.cost)
                 open.put((float(trans.total_cost) + float(trans.node.h_value), trans.node, trans.path,
                           float(trans.total_cost)))
-                open_dict[trans.node.name]=trans.node.name
+                open_dict[trans.node.name] = trans.node.name
     return False, False
 
 
@@ -227,10 +190,10 @@ def optimistic_f(end, transitions, heuristic_path):
     print("# HEURISTIC-OPTIMISTIC " + heuristic_path)
     conclusion = ""
     for state in transitions:
-        n, visited = ucs(state, transitions, end)
-        condition = ("OK" if float(state.h_value) <= float(n.total_cost) else "ERR")
-        print("[CONDITION]: [" + condition + "] h(" + state.name + ") <= h*: " + str(
-            float(state.h_value)) + " <= " + str(float(n.total_cost)))
+        n, visited = ucs(transitions[state], end)
+        condition = ("OK" if float(transitions[state].h_value) <= float(n.total_cost) else "ERR")
+        print("[CONDITION]: [" + condition + "] h(" + transitions[state].name + ") <= h*: " + str(
+            float(transitions[state].h_value)) + " <= " + str(float(n.total_cost)))
         if condition == "ERR":
             conclusion = " not"
     print("[CONCLUSION]: Heuristic is" + conclusion + " optimistic.")
@@ -240,19 +203,72 @@ def consistent_f(heuristic_path, transitions, heuristic_values):
     print("# HEURISTIC-CONSISTENT " + heuristic_path)
     conclusion = ""
     for state in transitions:
-        for neigbor in sorted(state.neigbours):
-            condition = ("OK" if float(heuristic_values[state.name]) <= float(neigbor.h_value) + float(
-                neigbor.cost) else "ERR")
-            print("[CONDITION]: [" + condition + "] h(" + state.name + ") <= h(" + neigbor.name + ") + c: " + str(
-                float(heuristic_values[state.name])) + " <= " + str(float(neigbor.h_value)) + " + " + str(
-                float(neigbor.cost)))
+        for neighbor in sorted(transitions[state].neighbours):
+            condition = (
+                "OK" if float(heuristic_values[transitions[state].name]) <= float(neighbor.node.h_value) + float(
+                    neighbor.cost) else "ERR")
+            print("[CONDITION]: [" + condition + "] h(" + transitions[
+                state].name + ") <= h(" + neighbor.node.name + ") + c: " + str(
+                float(heuristic_values[transitions[state].name])) + " <= " + str(
+                float(neighbor.node.h_value)) + " + " + str(
+                float(neighbor.cost)))
             if condition == "ERR":
                 conclusion = " not"
     print("[CONCLUSION]: Heuristic is" + conclusion + " consistent.")
 
+def dijkstra(source, transitions, distance):
+    queue = PriorityQueue()
 
-def algoritam_output(n, visited):
-    if n != False:
+    # for key in transitions:
+    #     for neighbor in transitions[key].neighbours:
+    #         newTransition = Transition(transitions[key],neighbor.cost,0,'')
+    #         if neighbor.node.neighbours and transitions[key].name != neighbor.node.name:
+    #             neighbor.node.neighbours.append(newTransition)
+    #         else:
+    #             new = [newTransition]
+    #             neighbor.node.neighbours=new
+
+    if not distance:
+        for state in transitions:
+            distance[transitions[state].name] = np.inf
+            if transitions[state].name == source:
+                distance[source] = 0
+            queue.put((distance[transitions[state].name], transitions[state]))
+
+    else:
+        for key in distance:
+            queue.put((float(distance[key]),transitions[key]))
+
+    while queue.qsize() != 0:
+        n = queue.get()[1]
+        if  len(n.neighbours) != 0:
+            expand = n.neighbours
+
+            for neighbour in expand:
+                new_distance = float(distance[n.name]) + float(neighbour.cost)
+                if new_distance < distance[neighbour.node.name]:
+                    distance[neighbour.node.name] = new_distance
+
+    return distance
+
+def optimistic_f2(end, transitions, heuristic_path):
+    print("# HEURISTIC-OPTIMISTIC " + heuristic_path)
+    conclusion = ""
+    distance = {}
+    for state in end:
+        distance = dijkstra(state, transitions, distance)
+
+    for state in transitions:
+        condition = ("OK" if float(transitions[state].h_value) <= float(distance[transitions[state].name]) else "ERR")
+        print("[CONDITION]: [" + condition + "] h(" + transitions[state].name + ") <= h*: " + str(
+            float(transitions[state].h_value)) + " <= " + str(float(distance[transitions[state].name])))
+        if condition == "ERR":
+            conclusion = " not"
+    print("[CONCLUSION]: Heuristic is" + conclusion + " optimistic.")
+
+
+def algorithm_output(n, visited):
+    if n:
         print("[FOUND_SOLUTION]: yes")
         print("[STATES_VISITED]: " + str(len(visited)))  # str(visited.qsize()))
         print("[PATH_LENGTH]: " + str(len(n.path.split("=>"))))
@@ -265,11 +281,13 @@ def algoritam_output(n, visited):
 def main():
     heuristic_values = {}
     optimistic, consistent = False, False
-    algoritam = ""
+    algorithm = ""
+    path = ''
+    heuristic_path = ''
     for x in range(1, len(sys.argv)):
         if sys.argv[x] == "--alg":
-            x = x + 1;
-            algoritam = sys.argv[x]
+            x = x + 1
+            algorithm = sys.argv[x]
         elif sys.argv[x] == "--ss":
             x = x + 1
             path = sys.argv[x]
@@ -283,23 +301,25 @@ def main():
             consistent = True
 
     start, end, transitions = transition(path, heuristic_values)
-    transitions = sorted(transitions)
+    transitions = collections.OrderedDict(sorted(transitions.items()))
     n, visited = False, False
-    if algoritam:
-        if algoritam == "astar":
+    if algorithm:
+        if algorithm == "astar":
             print("# A-STAR " + heuristic_path)
-            n, visited = astar(start, transitions, end, heuristic_values)
-        if algoritam == "bfs":
+            n, visited = astar(start, end)
+        if algorithm == "bfs":
             print("# BFS")
-            n, visited = bfs(start, transitions, end)
-        if algoritam == "ucs":
+            n, visited = bfs(start, end)
+        if algorithm == "ucs":
             print("# UCS")
-            n, visited = ucs(start, transitions, end)
-        algoritam_output(n, visited)
+            n, visited = ucs(start, end)
+        algorithm_output(n, visited)
 
     if optimistic:
         optimistic_f(end, transitions, heuristic_path)
 
     if consistent:
         consistent_f(heuristic_path, transitions, heuristic_values)
+
+
 main()
